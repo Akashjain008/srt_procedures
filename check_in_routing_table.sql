b39c0c0cfb4c6d13d1f77f052ebab568686b7c52
B2XCARE\AkashJain <akash.jain@b2x.com>
2018-04-02 16:06:28 +0530

    new commit

---
+++ check_in_routing_table.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE FUNCTION public.check_in_routing_table(tablename text, columnname text, checkvalues text[])
+ RETURNS TABLE("isValid" integer)
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+valueCount integer;
+dbCount integer;
+inValues text[];
+runQuery text;
+BEGIN
+
+	raise notice '%', checkValues;
+	drop table if exists tmp_is_in_routing;	
+	create temp table tmp_is_in_routing(b2x_code text);
+	insert into tmp_is_in_routing(b2x_code)
+	select unnest(checkValues);
+	
+	valueCount := (select count(distinct b2x_code) from tmp_is_in_routing);
+	
+	runQuery := format('select count(distinct %s) from %s where is_active = TRUE AND lower(%s::text) IN (select lower(b2x_code) from tmp_is_in_routing) ',columnName, tableName, columnName);
+	raise notice '%', runQuery;
+			
+	execute runQuery into dbCount; 
+	
+	raise notice 'dbcount: %, inpCount: %', dbCount, valueCount;
+	if(dbCount =  valueCount) then
+		RETURN QUERY select 1;
+	else
+		RETURN QUERY select 0;
+	end if;
+	
+END;
+$function$
