a4c383bb20acaf2f125431e986d9efedfc961c54
B2XCARE\AkashJain <akash.jain@b2x.com>
2018-04-03 15:49:57 +0530

    commit message

--- get_rule_dropdown.sql
+++ get_rule_dropdown.sql
@@ -5,7 +5,7 @@ AS $function$
 
 begin
 	"errorTypeData":= array(SELECT row_to_json(r) 
-				FROM(SELECT distinct rr.type as "errorType" FROM tr_rule rr where rr.type is not null or rr.type != 'null' )r);
+				FROM(SELECT distinct rr.type as "errorType1" FROM tr_rule rr where rr.type is not null or rr.type != 'null' )r);
 				
     "eventTypeData":= array(SELECT row_to_json(r) 
 				FROM(SELECT DISTINCT event_type as "eventType" FROM tr_rule  where event_type is not null or event_type != 'null')r);
