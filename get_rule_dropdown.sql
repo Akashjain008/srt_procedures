0bb58913b5002a51e82317cb8e412e8f9ee1b9d7
B2XCARE\AkashJain <akash.jain@b2x.com>
2018-04-03 15:25:00 +0530

    commit message

---
+++ get_rule_dropdown.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE FUNCTION public.get_rule_dropdown(OUT "errorTypeData" json[], OUT "eventTypeData" json[])
+ RETURNS record
+ LANGUAGE plpgsql
+AS $function$
+
+begin
+	"errorTypeData":= array(SELECT row_to_json(r) 
+				FROM(SELECT distinct rr.type as "errorType" FROM tr_rule rr where rr.type is not null or rr.type != 'null' )r);
+				
+    "eventTypeData":= array(SELECT row_to_json(r) 
+				FROM(SELECT DISTINCT event_type as "eventType" FROM tr_rule  where event_type is not null or event_type != 'null')r);
+
+	end;			
+
+$function$
